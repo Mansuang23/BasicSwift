7ee2c815f51b4cf926adfb23c8804fd834f9f84d
Com 05 <com05@Coms-iMac.local>
2017-07-25 14:24:52 +0700

    Print

--- Operated.playground/Contents.swift
+++ Operated.playground/Contents.swift
@@ -7,10 +7,23 @@ var strNumber: String = "5" // เป็น String ต้องใส่ " "
 
 var intNumber: Int = 20 // ไม่ต้องใส่ " "
 
-let intNewNumber = Int(strNumber) // Change Strint ==> Int การเปลี่ยนชนิดของข้อมูลจากอักษรเป็นตัวเลข
+//let intNewNumber = Int(strNumber) // Change Strint ==> Int การเปลี่ยนชนิดของข้อมูลจากอักษรเป็นตัวเลข
 
-let intAnswer = intNewNumber + intNumber
+//let intAnswer = intNewNumber + intNumber
 
-let intAnswer2 = intAnswer/intNewNumber
+//let intAnswer2 = intAnswer/intNewNumber
 
+let strName = "Mansuang"
+let strSurname = "Sitthipichaivong"
+let strSpace = " "
 
+let strGeneral = strName + strSpace + strSurname
+
+print(strGeneral)
+
+let douNumber1 = 12.34
+let douNumber2 = 10.5
+
+let douAnswer = douNumber1 + douNumber2
+
+print("คำตอบของ \(douNumber1) + \(douNumber2) = \(douAnswer)")
