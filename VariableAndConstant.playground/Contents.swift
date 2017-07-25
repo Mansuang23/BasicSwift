0aebee4eb9512ab85b2eb36a98b1a3f004b97245
Com 05 <com05@Coms-iMac.local>
2017-07-25 13:53:51 +0700

    DataType

--- VariableAndConstant.playground/Contents.swift
+++ VariableAndConstant.playground/Contents.swift
@@ -14,3 +14,26 @@ strName = "Nopita"
 // Implicit Variable Type let คือการประกาศค่าคงที่ ที่ไม่สามารถแก้ไขได้
 let strSurname = "ABC"
 // strSurname = "DEF"  // Error
+
+// Integer
+let intNumber = 123
+// intNumber = 456
+
+// Double
+let douNumber = 3.1416
+
+// Boolean
+let bolStatus = true // ต้องเปลี่ยน let เป็น var
+// bolStatus = false // Error
+
+
+// Explicit คือการกำหนด Data Type ==> Variable
+let strStudent: String = "Somchai"
+
+// let intNamber2: Int = "1" // ถ้าเป็นตัวเลขไม่ต้องใส่ฟันหนู เพราะเป็น int
+
+var douNumber2: Double = 123.456
+
+douNumber2 = 456.234
+
+let bolStatus2: Bool = false
